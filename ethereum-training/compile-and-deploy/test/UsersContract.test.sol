8839f3b2edf7a61d7664b4300526eca75b4de3bb
J. Carlos <50453088+jcib@users.noreply.github.com>
2019-06-03 13:17:09 +0200

    Update UsersContract.test.sol

--- ethereum-training/compile-and-deploy/test/UsersContract.test.sol
+++ ethereum-training/compile-and-deploy/test/UsersContract.test.sol
@@ -1,4 +1,5 @@
 const assert = require('assert'); //para que coincidan los valores proporcionados
+const AssertionError = require('assert').AssertionError;
 const Web3 = require('web3');
 
 const provider = new Web3.providers.HttpProvider("HTTP://127.0.0.1:7545"); //indicar la ruta
@@ -22,5 +23,78 @@ describe('The UsersContract', async() => {
         console.log(usersContract.options.address);
         assert.ok(usersContract.options.address);
     });
+
+    it('should join a user', async() => {
+        let name = "José";
+        let surName = "Carlos";
+
+        await usersContract.methods.join(name, surName)
+            .send({from: accounts[0], gas: '500000'});
+    });
+
+    it('should retrieve a user', async() => {
+        let name = "José";
+        let surName = "Carlos";
+
+        await usersContract.methods.join(name,surName)
+            .send({from: accounts[0], gas: '500000'});
+
+        let user = await usersContract.methods.getUser(accounts[0]).call();
+
+        assert.equal(name, user[0]);
+        assert.equal(surName, user[1]);
+
+    });
+
+    it('should not allow joining an account twice', async() => {
+
+        await usersContract.methods.join("Pedro","Lopez")
+            .send( {from: accounts[1], gas: '500000'});
+
+        try{
+            await usersContract.methods.join("Ana","Gomez")
+                .send( {from: accounts[1], gas: '500000'});
+            assert.fail('same account cant join twice');
+        }
+        catch(e){
+            if(e instanceof AssertionError){
+                assert.fail(e.message);
+            }
+        }
+    });
+    
+    it('should not allow retrieving a not registered user', async() => {
+
+        try {
+
+            await usersContract.getUser(accounts[0]).call();
+            assert.fail('user should not be registered');
+
+        }
+
+        catch(e) {
+
+            if (e instanceof AssertionError ) {
+
+                assert.fail(e.message);
+
+            }
+
+        }
+    });
+
+    
+    it('should retrieve total registered users', async () => {
+
+        await usersContract.methods.join("Ana", "Gomez")
+            .send({ from: accounts[0], gas: '500000' });
+
+        await usersContract.methods.join("Mario", "Bros")
+            .send({ from: accounts[1], gas: '500000' });
+
+        let total = await usersContract.methods.totalUsers().call();        
+        assert.equal(total, 2);        
+    });
+
 
 });
